3651305bcd89059068510d63bc844938025991e5
dmand1c <damirmandicgit@gmail.com>
2020-11-20 11:21:02 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ...
+  plugins: [
+    // require('@tailwindcss/ui'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ]
+}
